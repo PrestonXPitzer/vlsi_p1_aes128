a0fdffd0c1c323547f87a56fed9bc7e2f2de7ce2
Nate <nate03@vt.edu>
2025-09-24 16:49:27 -0400

    progress on word by word key expansion

--- key_expand.sv
+++ key_expand.sv
@@ -13,21 +13,21 @@ module key_expand(
     logic [1:0]   load_count;   // counts which 32-bit word we are on
     logic         loading;      // high when we are loading the key
     logic [127:0] round_keys [0:10]; // 2D-aray : 11 round keys (0 = initial key, 10 = last)
-    logic         expanded;     // high when expansion is done
 
+
+    logic [31:0] w [0:43]; // 44 words total (AES-128)
     // Key loading state machine
     always @(posedge clk or posedge reset) begin
         if (reset) begin
             key_reg    <= 128'd0;
             load_count <= 2'd0;
             loading    <= 1'b0;
-            expanded   <= 1'b0;
         end else begin
             if (start) begin
                 load_count <= 2'd0;
                 loading    <= 1'b1;
-                expanded   <= 1'b0;
-            end else if (loading) begin
+            end 
+            else if (loading) begin
                 case (load_count)
                     2'd0: key_reg[127:96] <= cipher_key;
                     2'd1: key_reg[95:64]  <= cipher_key;
@@ -36,9 +36,9 @@ module key_expand(
                 endcase
                 if (load_count == 2'd3) begin
                     loading  <= 1'b0;
-                    expanded <= 1'b0;
+                    load_count <= 2'b00;
                 end
-                load_count <= load_count + 1'b1;
+                else load_count <= load_count + 1'b1;
             end
         end
     end
@@ -48,8 +48,9 @@ module key_expand(
     typedef enum logic [1:0] {IDLE, LOAD, EXPAND, DONE} state_t;
     state_t state;
 
-    logic [3:0] round_ctr;
+    logic [5:0] round_ctr; //edited
     logic [127:0] current_key;
+
 
     //key expansion code
     always_ff @(posedge clk or posedge reset) begin
@@ -57,36 +58,57 @@ module key_expand(
             state <= IDLE;
             round_ctr <= 0;
             done <= 0;
-        end else begin
+        end 
+        else if (start)begin 
+            state   <= LOAD;
+            done <= 0;
+        end
+        else begin
             case (state)
-                IDLE: if (!loading) begin
-                    current_key <= key_reg;  // initial key
-                    round_keys[0]   <= key_reg;
-                    round_ctr   <= 0;
-                    state       <= EXPAND;
+                IDLE: begin
+                    state <= IDLE;
                 end
-                EXPAND: begin
-                    //done after 10 cycles
-                    if (round_ctr <= 9) begin
-                        // generate next_key from current_key
-                        current_key <= next_key(current_key, round_ctr+1);
-                        round_keys[round_ctr+1] <= next_key(current_key, round_ctr+1);
-                        round_ctr <= round_ctr + 1;
+                LOAD:begin
+                    if (!loading) begin
+                        done <= 0;
+                        current_key <= key_reg;  // initial key
+                        round_keys[0]   <= key_reg;
+                        w[0] <= key_reg[127:96];
+                        w[1] <= key_reg[95:64];
+                        w[2] <= key_reg[63:32];
+                        w[3] <= key_reg[31:0];
+                        round_ctr   <= 4;
+                        state       <= EXPAND;
                     end 
-                    else state <= DONE;
+                end
+                // EXPAND: begin
+                //     //done after 10 cycles
+                //     if (round_ctr <= 9) begin
+                //         // generate next_key from current_key
+                //         //*should edit to compute next key once into temp*
+                //         current_key <= next_key(current_key, round_ctr+1);
+                //         round_keys[round_ctr+1] <= next_key(current_key, round_ctr+1);
+                //         round_ctr <= round_ctr + 1;
+                //     end 
+                //     else state <= DONE;
+                // end
+                EXPAND: begin
+                    if (round_ctr <= 43) begin
+                        w[round_ctr] <= next_word(round_ctr, w[round_ctr-1], w[round_ctr-4], round_ctr>>2); //(>>2 equiv to divide by 4)
+                        round_ctr <= round_ctr + 1;
+                    end else begin
+                        // pack round keys into 128-bit blocks
+                        for (int r = 0; r <= 10; r++) begin
+                            round_keys[r] <= {w[4*r], w[4*r+1], w[4*r+2], w[4*r+3]};
+                        end
+                        state <= DONE;
+                    end
                 end
                 DONE: begin
                     done <= 1;
-                    state <= IDLE;
+                    state <= IDLE; //stay in done until new start pulse
                 end
             endcase
-        end
-
-        //in case of another start pulse
-        if(loading) begin
-            done <= 1'd0;
-            round_ctr <= 0;
-            state <= IDLE;
         end
     end
         
@@ -108,26 +130,42 @@ module key_expand(
 
     //helper functions
     // Function to generate the next round key
-    function automatic logic [127:0] next_key(
-            input logic [127:0] prev_key,
-            input int round_num
-        );
-            logic [31:0] temp, w0, w1, w2, w3;
-        begin
-            // pull last word of previous round key
-            temp = get_word(prev_key, 3);
+    // function automatic logic [127:0] next_key(
+    //         input logic [127:0] prev_key,
+    //         input int round_num
+    //     );
+    //         logic [31:0] temp, w0, w1, w2, w3;
+    //     begin
+    //         // pull last word of previous round key
+    //         temp = get_word(prev_key, 3);
 
-            // Special schedule core every 4th word
+    //         // Special schedule core every 4th word
+    //         temp = sub_word(rot_word(temp)) ^ rcon(round_num);
+
+    //         // Build new round key word by word
+    //         w0 = get_word(prev_key, 0) ^ temp;
+    //         w1 = get_word(prev_key, 1) ^ w0;
+    //         w2 = get_word(prev_key, 2) ^ w1;
+    //         w3 = get_word(prev_key, 3) ^ w2;
+
+    //         return {w0, w1, w2, w3};
+    //     end
+    // endfunction
+
+    function automatic logic [31:0] next_word(
+    input int i,
+    input logic [31:0] prev_word,
+    input logic [31:0] wordNk,
+    input int round_num
+    );
+        logic [31:0] temp;
+    begin
+        temp = prev_word;
+        if (i % 4 == 0) begin
             temp = sub_word(rot_word(temp)) ^ rcon(round_num);
-
-            // Build new round key word by word
-            w0 = get_word(prev_key, 0) ^ temp;
-            w1 = get_word(prev_key, 1) ^ w0;
-            w2 = get_word(prev_key, 2) ^ w1;
-            w3 = get_word(prev_key, 3) ^ w2;
-
-            return {w0, w1, w2, w3};
         end
+        next_word = wordNk ^ temp;
+    end
     endfunction
 
     // RotWord: rotate left by 8 bits
